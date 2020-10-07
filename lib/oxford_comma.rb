17d5dee1cb676e664f564a0a688dd4cb88704ffe
Matt Smith <mrandolph83@gmail.com>
2020-10-07 15:34:26 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
 
+  if array.size == 1
+    return array[0]
+
+  elsif array.size == 2
+    return array.join(" and ")
+
+  else array.size <= 3
+   return array[0..-2].join(', ') + ", and " + array[-1]
+  end
+
 end
