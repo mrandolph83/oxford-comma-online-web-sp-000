0311d74b0144eb5af59919f83ceaed53e401e86b
Matt Smith <mrandolph83@gmail.com>
2020-10-07 15:26:14 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -6,10 +6,8 @@ def oxford_comma(array)
   elsif array.size == 2
     return array.join(" and ")
 
-  else array.size == 3
-    return array.join(" , ")
-
-
+  else array.size <= 3
+   return array[0..-2].join(', ') + ", and " + array[-1]
   end
 
 end
